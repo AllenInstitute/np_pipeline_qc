9cea23b84a332bcc5c818dbf0ada1841ac1b7766
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-03-19 21:29:59 -0700

    Parse input args

--- src/np_pipeline_qc/__main__.py
+++ src/np_pipeline_qc/__main__.py
@@ -1,6 +1,12 @@
 import argparse
-import pathlib
 
-parser = argparse.ArgumentParser()
-parser.add_argument('session', type=pathlib.Path, required=True, help='A lims session ID, or a string/path containing one')
-args = parser.parse_args()
+import np_session
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('session', type=np_session.Session, help='A lims session ID, or a string/path containing one')
+    session = parser.parse_args().session
+
+    print(f'Running QC for {session} | {"Hab" if session.is_hab else "Ephys"} | {session.project}')
+    print('[not implemented]')
