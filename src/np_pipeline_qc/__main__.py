ba7bed86f15a9bee865e4dd924675e4a0147e22a
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-03-20 18:17:55 -0700

    Start new QC classes module

--- src/np_pipeline_qc/__main__.py
+++ src/np_pipeline_qc/__main__.py
@@ -1,12 +1,27 @@
 import argparse
 
+import np_logging
 import np_session
+
+from np_pipeline_qc.classes import BaseQC
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('session', type=np_session.Session, help='A lims session ID, or a string/path containing one')
-    session = parser.parse_args().session
+    logger = np_logging.getLogger()
 
-    print(f'Running QC for {session} | {"Hab" if session.is_hab else "Ephys"} | {session.project}')
-    print('[not implemented]')
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'session',
+        type=np_session.Session,
+        help='A lims session ID, or a string/path containing one',
+    )
+    session = parser.parse_args(
+        tuple(str(np_session.Projects.TTN.get_latest_ephys()))
+    ).session
+
+    logger.info(
+        f'Running QC for {session} | {"Hab" if session.is_hab else "Ephys"} | {session.project}'
+    )
+
+    # make QC-class factory
+    BaseQC(session).run()
