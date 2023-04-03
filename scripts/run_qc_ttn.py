61bfcf9bff43e66116c7869fd67bdceb2077dd20
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-04-03 14:19:43 -0700

    Run latest TTN session

--- scripts/run_qc_ttn.py
+++ scripts/run_qc_ttn.py
@@ -12,7 +12,7 @@ import np_pipeline_qc
 if __name__ == '__main__':
     logger = np_logging.getLogger()
     sessions = sorted(np_session.sessions(project='TTN'))
-    np_pipeline_qc.run_qc(sessions[-1], modules_to_run=('all',), debug=False)
+    np_pipeline_qc.run_qc(sessions[-1], debug=False)
     exit()
     
     for session in np_session.sessions(project='TTN'):
