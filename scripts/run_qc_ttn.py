1479b55a8c53ee69b4781d6a34eb8341ff1f6cc8
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-04-03 13:51:36 -0700

    Run latest TTN session

---
+++ scripts/run_qc_ttn.py
@@ -0,0 +1,21 @@
+import time
+from matplotlib import pyplot as plt
+
+import np_logging
+import np_session
+
+import np_pipeline_qc
+
+
+# plt.style.use('seaborn-v0_8-whitegrid')
+
+if __name__ == '__main__':
+    logger = np_logging.getLogger()
+    sessions = sorted(np_session.sessions(project='TTN'))
+    np_pipeline_qc.run_qc(sessions[-1], modules_to_run=('all',), debug=False)
+    exit()
+    
+    for session in np_session.sessions(project='TTN'):
+        if session.mouse == 366122:
+            continue
+        np_pipeline_qc.run_qc(session, modules_to_run=('all',), debug=True)
